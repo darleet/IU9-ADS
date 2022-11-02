72651e9b1eb0a2408a451813c1dc981d4d8fe58a
darleet <darleet@mail.ru>
2022-11-02 15:35:11 +0300

    Initial commit

---
+++ Informatics Basics/Lab works/unit-test.rkt
@@ -0,0 +1,24 @@
+(define-syntax test
+  (syntax-rules ()
+    ((_ expr expected-result)
+     (list (quote expr) expected-result)))) ; Преобразование в list
+
+(define ie (interaction-environment))
+
+(define (run-tests tests)
+  (define (loop correct? tests)
+    (if (null? tests)
+        correct?
+        (let ((expr (caar tests)))
+          (let* ((result (eval expr ie))
+                 (check (equal? result (cadar tests))))
+            (write expr)
+            (if check
+                (begin
+                  (display " ok") (newline))
+                  (begin
+                    (display " FAIL") (newline)
+                    (display "  Expected: ") (write (cadar tests)) (newline)
+                    (display "  Returned: ") (write result) (newline)))
+                (loop (and correct? check) (cdr tests))))))
+    (loop #t tests))
