5859759a6a74fc0ba255f8bbc5295cc7dd254ee6
darleet <darleet@mail.ru>
2022-11-16 04:03:42 +0300

    Updated 2nd homework.

--- Informatics Basics/Home/2dz.rkt
+++ Informatics Basics/Home/2dz.rkt
@@ -181,4 +181,63 @@
               '()))))
     (loop '() l-tail '())))
 
-  
+
+(define (string-prefix? a b)
+  (let ((la (string->list a))
+        (lb (string->list b)))
+    (define (loop la lb status)
+      (cond ((or (null? la) (not status)) status) ; конец первой строки
+            ((null? lb) (not status)) ; конец второй строки
+            (else (loop (cdr la) (cdr lb) (equal? (car la) (car lb))))))
+    (loop la lb #t)))
+
+
+(define (string-suffix? a b)
+  (let ((la (string->list a))
+        (lb (string->list b)))
+    (define (loop buff-la lb)
+      (cond ((null? lb) ; закончились обе строки? (если да => первая в конце второй)
+             (null? buff-la))
+            ((or (null? buff-la) (not (equal? (car buff-la) (car lb))))
+             (loop la (cdr lb))) ; если элементы не равны или буфер закончился
+            (else
+             (loop (cdr buff-la) (cdr lb)))))
+    (loop la lb)))
+
+
+(define (string-infix? a b)
+  (let ((la (string->list a))
+        (lb (string->list b)))
+    (define (loop buff-la lb)
+      (cond ((or (null? buff-la) (null? lb))
+             (null? buff-la))
+            ((equal? (car buff-la) (car lb))
+             (loop (cdr buff-la) (cdr lb)))
+            (else
+             (loop la (cdr lb)))))
+    (loop la lb)))
+
+(define (string-list char)
+  (list (list->string (list char))))
+
+(define (string-split str sep)
+  (let ((lstr (string->list str))
+        (lsep-init (string->list sep)))
+    (define (loop buff-lsep lstr lsep res)
+      (cond ((null? lstr)
+             (append res buff-lsep))
+            ((null? lsep)
+             (loop '() (cdr lstr) lsep-init (append res (string-list (car lstr)))))
+            ((equal? (car lstr) (car lsep))
+             (loop
+              (append buff-lsep (string-list (car lsep)))
+              (cdr lstr)
+              (cdr lsep)
+              res))
+            (else
+             (loop
+              '()
+              (cdr lstr)
+              lsep-init
+              (append res buff-lsep (string-list (car lstr)))))))
+    (loop '() lstr lsep-init '())))
