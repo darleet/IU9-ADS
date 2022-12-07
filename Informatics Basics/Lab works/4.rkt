1f579b70038cbb82e4c627aed1271feb2f479f10
darleet <darleet@mail.ru>
2022-12-07 10:39:44 +0300

    Added 2nd task from 4th Lab Work

--- Informatics Basics/Lab works/4.rkt
+++ Informatics Basics/Lab works/4.rkt
@@ -43,5 +43,34 @@
 (define (line-count path)
   (call-with-input-file path
     (lambda (port)
-      (define (loop 
+      (define (loop counter prev-newline?)
+        (let ((symbol (read-char port)))
+          (cond ((and (equal? symbol #\newline) (not prev-newline?))
+                 (loop (+ 1 counter) (equal? symbol #\newline)))
+                ((eof-object? symbol) counter)
+                (else (loop counter (equal? symbol #\newline))))))
+      (loop 0 #\newline))))
       
+(line-count "1.rkt")
+
+
+;---3---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
