a0ca53e42d968e42ededabdd361a4d8f8713d5b1
darleet <darleet@mail.ru>
2022-12-27 16:45:09 +0300

    Changed the structure. Added final IB Labs

--- Informatics Basics/Lab works/Lab4/my-if.rkt
+++
@@ -1,9 +0,0 @@
-(define-syntax my-if
-  (syntax-rules ()
-    ((_ predicate then else)
-     (let ((a (delay then))
-           (b (delay else)))
-       (or (and predicate (force a)) (force b))))))
-
-(my-if #t 1 (/ 1 0))
-(my-if #f (/ 1 0) 1)
