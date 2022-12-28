5d0f1f422c5b2721378b2ff00f2058897da9fe33
darleet <darleet@mail.ru>
2022-12-28 15:11:30 +0300

    Small changes

--- Basics/Labs/6.scm
+++ Basics/Labs/6.scm
@@ -51,7 +51,6 @@
 
 (run-tests check-frac-tests)
 
-
 #| --scan-frac grammar--
 <Expression>      ::= <Signed-Num> "/" <Unsigned-Num>
 <Signed-Num>      ::= "+" <Unsigned-Num> | "-" <Unsigned-Num> | <Unsigned-Num>
@@ -115,16 +114,15 @@
 (define (scan-many-fracs str)
   (define stream (make-stream (string->list str) 'eos))
   (define (expr-list stream error)
+    (spaces stream error) ; Убрали пробелы перед дробью
     (if (and (char? (peek stream))
              (or (char-whitespace? (peek stream))
                  (char-numeric? (peek stream))
                  (equal? (peek stream) #\+)
                  (equal? (peek stream) #\-)))
-        (begin
-          (spaces stream error) ; Убрали пробелы перед дробью
-          (let ((expr-new (expr stream error)))
-            (spaces stream error) ; Убрали пробелы после дроби
-            (cons expr-new (expr-list stream error))))
+        (let ((expr-new (expr stream error)))
+          (spaces stream error) ; Убрали пробелы после дроби
+          (cons expr-new (expr-list stream error)))
         '()))
   (define (expr stream error)
     (define numerator (signed-num stream error))
@@ -176,6 +174,7 @@
 
 (define (parse tokens)
   (define stream (make-stream (vector->list tokens) "eof"))
+  (define reserved-words '(define end endif))
   (define (expect term stream error)
     (if (equal? (peek stream) term)
         (next stream)
@@ -191,7 +190,9 @@
            (_word (next stream))
            (_body (body stream error))
            (_end (expect 'end stream error)))
-      (list _word _body)))
+      (if (not (member _word reserved-words))
+          (list _word _body)
+          (error #f))))
   (define (body stream error)
     (cond ((equal? (peek stream) 'if)
            (let* ((_if (next stream))
@@ -221,26 +222,26 @@
         (test (parse #(x dup 0 swap if drop -1 endif))
               '(() (x dup 0 swap (if (drop -1)))))
         (test (parse #( define -- 1 - end
-                        define =0? dup 0 = end
-                        define =1? dup 1 = end
-                        define factorial
-                        =0? if drop 1 exit endif
-                        =1? if drop 1 exit endif
-                        dup --
-                        factorial
-                        *
-                        end
-                        0 factorial
-                        1 factorial
-                        2 factorial
-                        3 factorial
-                        4 factorial ))
+                         define =0? dup 0 = end
+                         define =1? dup 1 = end
+                         define factorial
+                         =0? if drop 1 exit endif
+                         =1? if drop 1 exit endif
+                         dup --
+                         factorial
+                         *
+                         end
+                         0 factorial
+                         1 factorial
+                         2 factorial
+                         3 factorial
+                         4 factorial ))
               '(((-- (1 -))
-                (=0? (dup 0 =))
-                (=1? (dup 1 =))
-                (factorial
-                 (=0? (if (drop 1 exit)) =1? (if (drop 1 exit)) dup -- factorial *)))
-               (0 factorial 1 factorial 2 factorial 3 factorial 4 factorial)))
+                 (=0? (dup 0 =))
+                 (=1? (dup 1 =))
+                 (factorial
+                  (=0? (if (drop 1 exit)) =1? (if (drop 1 exit)) dup -- factorial *)))
+                (0 factorial 1 factorial 2 factorial 3 factorial 4 factorial)))
         (test (parse #(define word w1 w2 w3))
               #f)
         (test (parse #(0 if 1 if 2 endif 3 endif 4))
@@ -254,18 +255,24 @@
                         90 99 gcd
                         234 8100 gcd))
               '(((=0? (dup 0 =))
-                (gcd (=0?
-                      (if (drop exit))
-                      swap over mod
-                      gcd)))
-               (90 99 gcd
-                234 8100 gcd)))))
+                 (gcd (=0?
+                       (if (drop exit))
+                       swap over mod
+                       gcd)))
+                (90 99 gcd
+                    234 8100 gcd)))))
 
 (run-tests parse-tests)
 
 
+(define another-tests
+  (list (test (scan-many-fracs "") '())
+        (test (scan-many-fracs " ") '())
+        (test (parse #(endif)) #f)
+        (test (parse #(define define end)) #f)
+        (test (parse #(define endif end)) #f)))
 
-
+(run-tests another-tests)
 
 
 
