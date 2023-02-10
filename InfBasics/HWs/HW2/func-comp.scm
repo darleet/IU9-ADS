3d01cd751164e72f19717d032167cc61a8b466f7
darleet <darleet@mail.ru>
2023-02-10 23:49:05 +0300

    Structure changes

--- InfBasics/HWs/HW2/func-comp.scm
+++
@@ -1,9 +0,0 @@
-(define (o . procs)
-  (define (loop procs)
-    (if (null? procs)
-        (lambda (x) x)
-        (let ((proc (car procs)))
-          (if (null? (cdr procs))
-              proc
-              (lambda (x) (proc ((loop (cdr procs)) x)))))))
-  (loop procs))
